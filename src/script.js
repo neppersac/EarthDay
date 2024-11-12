e2725cfe92b93891401f88d100af6518b0dacb80
Francisco Soares <neppersac@yahoo.com.br>
2024-11-12 12:43:08 +0000

    graficos

--- src/script.js
+++ src/script.js
@@ -1,17 +1,19 @@
-// Seleciona todos os elementos <article> dentro da <section>
-const articles = document.querySelectorAll('section article');
+function atualizarDados() {
+    alert("Atualizando dados...");
+}
 
-// Array com o caminho das imagens correspondentes a cada <article>
-const images = ['images/logo.png', 'images/terra.jpg', 'images/faq.jpg'];
+function executarModelo() {
+    alert("Executando modelo...");
+}
 
-// Itera sobre cada <article> e adiciona a imagem correspondente
-articles.forEach((article, index) => {
-    // Cria um elemento <img>
-    const img = document.createElement('img');
-    // Define o atributo src da imagem com o caminho da imagem correspondente
-    img.src = images[index];
-    // Adiciona a classe para estilização
-    img.classList.add('article-image');
-    // Insere a imagem como o primeiro filho do <article>
-    article.insertBefore(img, article.firstChild);
-});
+function salvarConfiguracao() {
+    alert("Configuração salva.");
+}
+
+function downloadResultados() {
+    alert("Baixando resultados...");
+}
+
+function salvarAnotacao() {
+    alert("Anotação salva.");
+}
